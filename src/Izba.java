fc7103104c05d8d170c40c629e8cbc87bbdd7dd7
PopiJack <samuelpopjak@outlook.sk>
2023-05-25 17:09:34 +0200

    Priadnie svetla do izieb

--- src/Izba.java
+++ src/Izba.java
@@ -4,16 +4,28 @@ import java.util.ArrayList;
 
 public class Izba extends AktivnyPrvokMapy {
     private Obdlznik obdlznik;
+    private boolean jeTma;
 
     private ArrayList<AktivnyPrvokMapy> predmety;
 
     public Izba(int poziciaX, int poziciaY, int sirka, int vyska) {
         super(poziciaX, poziciaY, sirka, vyska, "izba", 0);
+        this.jeTma = false;
         this.obdlznik = new Obdlznik(super.getPoziciaX(), super.getPoziciaY());
         this.obdlznik.zmenStrany(super.getSirka(), super.getVyska());
-        this.obdlznik.zmenFarbu("blue");
+        this.obdlznik.zmenFarbu("black");
         this.obdlznik.zobraz();
         this.predmety = new ArrayList<>();
+    }
+
+    public void vytvorTmu() {
+        this.jeTma = true;
+        this.obdlznik.zobraz();
+    }
+
+    public void vypniSvetlo() {
+        this.jeTma = false;
+        this.obdlznik.skry();
     }
 
     public int getPocetPrvkov() {
